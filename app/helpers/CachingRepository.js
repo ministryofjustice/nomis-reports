e2a9b62fc1f535e85389248440d9a8033439c73e
Matt Smith <mattonfoot@gmail.com>
2018-01-09 13:51:06 +0000

    Caching tests

--- app/helpers/CachingRepository.js
+++ app/helpers/CachingRepository.js
@@ -1,46 +1,57 @@
 const fs = require('fs');
 const util = require('util');
 
-const timeout = 200;
-const root = `${__dirname}/../../.cache/`;
+const isObject = (x) => x != null && typeof x === 'object' && Array.isArray(x) === false;
 
 const unlink = util.promisify(fs.unlink);
 const mkdir = util.promisify(fs.mkdir);
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
-mkdir(root);
-
 const datastore = {
-  write(path, data) {
-    return writeFile(path, data, 'utf8');
+  root: `${__dirname}/../../.cache`,
+  timeout: 200,
+
+  put(cacheKey, data) {
+    let path = `${datastore.root}/${cacheKey}`;
+
+    return writeFile(path, data, 'utf8')
+      .then(() => setTimeout(() => unlink(path), datastore.timeout));
   },
 
-  read(path) {
+  get(cacheKey) {
+    let path = `${datastore.root}/${cacheKey}`;
+
     return readFile(path, 'utf8');
   }
 };
 
-const intercept = (cacheKey, ds) => (data) => {
-  let f = `${root}/${cacheKey}`;
+mkdir(datastore.root).catch(() => { /* dir exists */ });
 
-  ds.write(f, JSON.stringify(data), 'utf8')
-    .then(() => setTimeout(() => unlink(f), timeout));
+const cache = (cacheKey, ds) => (data) => {
+  if (data) {
+    ds.put(cacheKey, JSON.stringify(data));
+  }
 
   return data;
 };
 
+const callRemote = (repository, fn, args, cacheKey, ds) => () => {
+  return fn.apply(repository, args)
+    .then(cache(cacheKey, ds));
+};
+
 const checkCache = (cacheKey, ds) => {
-  return ds.read(`${root}/${cacheKey}`)
+  return ds.get(cacheKey)
     .then((data) => JSON.parse(data));
 };
 
 const wrap = (repository, method, prefix, ds) => (...args) => {
-  let cacheKey = [].concat([prefix, method], args).join(' ').trim().replace(/\s/g, '_');
+  let cacheKey = [].concat([prefix, method], args.map((x) => isObject(x) ? JSON.stringify(x) : x)).join(' ').trim().replace(/\s/g, '_');
   let fn = repository[method];
 
   return checkCache(cacheKey, ds)
-    .catch(() => fn.apply(repository, args).then(intercept(cacheKey, ds)));
+    .catch(callRemote(repository, fn, args, cacheKey, ds));
 };
 
 function CachingRepository(Repository, config, agent, ds) {
--- test/api/helpers/CachingRepository.js
+++ test/api/helpers/CachingRepository.js
@@ -44,11 +44,14 @@ describe('A Caching Repository', () => {
     let ds = {
       data: {},
 
-      read(path) {
+      get(path) {
+        path = path.replace(__dirname, '');
         return new Promise((resolve, reject) => this.data[path] ? resolve(this.data[path]) : reject(new Error('file does not exist')));
       },
 
-      write(path, data) {
+      put(path, data) {
+        path = path.replace(__dirname, '');
+
         this.data[path] = { path, data };
 
         return new Promise((resolve) => resolve(this.data[path]));
@@ -57,37 +60,34 @@ describe('A Caching Repository', () => {
 
     let cachingService = new LocationService(config, new CachingRepository(LocationRepository, config, request(server), ds));
 
-    it('should retrieve a list of locations from the remote', () =>
+    it('should store a list docuemnt in the cache', () =>
       cachingService.list()
-        .then((data) => {
-          should.exist(data);
-
-          data.should.eql(exampleSet);
-        })
-        .then(() => {
-          ds.data['../../.data/LocationRepository_list'].should.have.property('path', '../../.data/LocationRepository_list');
-
-          ds.data['../../.data/LocationRepository_list'].should.have.property('data');
-          ds.data['../../.data/LocationRepository_list'].data.should.eql(exampleSet);
-        })
+        .then((data) => should.exist(data) && data.should.eql(exampleSet))
+        .then(() => ds.data['LocationRepository_list'].should.have.property('data', JSON.stringify(exampleSet)))
     );
 
-/*
-    it('should retrieve a location from the remote for a known locationId', () =>
+    it('should store a response document in the cache', () =>
       cachingService.getDetails('TEST')
-        .then((data) => data.should.eql(exampleRecord)));
+        .then((data) => should.exist(data) && data.should.eql(exampleRecord))
+        .then(() => ds.data['LocationRepository_getDetails_TEST'].should.have.property('data', JSON.stringify(exampleRecord)))
+    );
 
-    it('should return nothing if the locationId is not known', () =>
+    it('should not create a cache object if the response was not OK', () =>
       cachingService.getDetails('VOID')
-        .then((data) => should.not.exist(data)));
+        .then((data) => should.not.exist(data))
+        .then(() => should.not.exist(ds.data['LocationRepository_getDetails_VOID']))
+    );
 
-    it('should retrieve a list of inmates from the remote for a known locationId', () =>
+    it('should include path params in the cachekey', () =>
       cachingService.listInmates('FOO')
-        .then((data) => data.should.eql(exampleInmateList)));
+        .then((data) => should.exist(data) && data.should.eql(exampleInmateList))
+        .then(() => ds.data['LocationRepository_listInmates_FOO'].should.have.property('data', JSON.stringify(exampleInmateList)))
+    );
 
-    it('should pass the search parameters to the remote', () =>
+    it('should include the search parameters in the cachekey', () =>
       cachingService.listInmates('ECHO', { q: 'BAR' })
-        .then((data) => data.should.eql({ query: { q: 'BAR' }})));
-*/
+        .then((data) => should.exist(data) && data.should.eql({ query: { q: 'BAR' }}))
+        .then(() => ds.data['LocationRepository_listInmates_ECHO_{"q":"BAR"}'].should.have.property('data', JSON.stringify({ query: { q: 'BAR' }})))
+    );
   });
 });
