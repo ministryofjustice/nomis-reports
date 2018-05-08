b566d9ebb93db507cfae775c7c0d145206ab5075
Matt Smith <mattonfoot@gmail.com>
2018-05-08 11:50:21 +0100

    use configured reports api in offence lists

--- app/repositories/OffenceRepository.js
+++ app/repositories/OffenceRepository.js
@@ -6,7 +6,7 @@ function OffenderRepository(config, agent) {
   this.config = Object.assign({ limit: 2000 }, config);
   this.agent = apiAgent(agent, [
       (request) => {
-        request.set('Authorization', 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnRlcm5hbFVzZXIiOmZhbHNlLCJzY29wZSI6WyJyZXBvcnRpbmciXSwiZXhwIjoxODM5MjUzMzczLCJhdXRob3JpdGllcyI6WyJST0xFX1JFUE9SVElORyJdLCJqdGkiOiI3ZjE0NTdlNi1hYTNiLTQzZWYtOTY3Zi1iZmY4YjgxZTEyMDYiLCJjbGllbnRfaWQiOiJhcGlyZXBvcnRpbmcifQ.lBPf6kUI840svb2ayTvV9jP7hOVFr7bU5ZzyEfqH8RIyMOutCnAcJAcsl4bqI96fId0Db15UEGUEoFMWTBArD17MoomS_U8P-jJxpwhHeHH2UEi_wAv6bLwVbbNvBSxWJ37TiHsPD-hr_63cS7qZs_8xi20dTjmAw8paSMn6jKKb8xUWJWLBAAi0rQZbyOfHva_9deF4W_wSix0oEc0r7CT321Sg93TrijoagJ1X5ha7MzF3wsOJEcvLmIDAAhSYM4ZcJT1ZUvqEd3YVHKrp_X-eqcsKFRmb3lY7IZ-fz10o26gFiagE7yYAb7fuykREiwlMUqzt0lw6K7Suiet5qA');
+        request.set('Authorization', `Bearer ${this.config.reports.bearerToken}`);
 
         return request;
       }
